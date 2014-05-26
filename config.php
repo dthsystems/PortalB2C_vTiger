11e157af31d6f2633a196b36694ccfc595be8af7
dthsystems <contact@dthsystems.com>
2014-05-26 16:45:45 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,73 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+
+ /*********************************************************************************
+
+ * Description:  Defines the Vietnamese language pack for the base application.
+ * Portions created by DTH Systems Inc.
+ * All Rights Reserved.
+ * Contributor(s): DTH Systems Inc (www.dthsystems.com)
+  * Contact : contact@dthsystems.com or Mobiles: 0989 068 581 ; 0912 265 766
+ ********************************************************************************/
+
+
+$languageStrings = array(
+	// Basic Strings
+	'Accounts' => 'Tổ chức',
+	'SINGLE_Accounts' => 'Tổ chức',
+	'LBL_ADD_RECORD' => 'Thêm mới tổ chức ',
+	'LBL_RECORDS_LIST' => 'Danh mục tổ chức',
+
+	// Blocks
+	'LBL_ACCOUNT_INFORMATION' => 'Thông tin liên hệ',
+
+	// Mass Action
+	'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Cấp bậc tổ chức',
+
+	//Field Labels
+	'industry' => 'Ngành nghề',
+	'Account Name' => 'Tên tổ chức',
+	'Account No' => 'Mã số',
+	'Website' => 'Website',
+	'Ticker Symbol' => 'Mã chứng khoán',
+	'Member Of' => 'Thành viên của',
+	'Employees' => 'Số lượng nhân viên',
+	'Ownership' => 'Người sở hữu',
+	'SIC Code' => 'Mã ngành kinh tế',
+	'Other Email' => 'Email khác',
+	'Other Phone' => 'Điện thoại khác',
+	'Phone' => 'Điện thoại',
+	'Email' => 'Email',
+	
+	//Added for existing picklist entries
+
+	'Analyst'=>'Nhà phân tích',
+	'Competitor'=>'Đối thủ',
+	'Customer'=>'Khách hàng',
+	'Integrator'=>'Người liên kết',
+	'Investor'=>'Nhà đầu tư',
+	'Press'=>'Báo chí',
+	'Prospect'=>'Triển vọng',
+	'Reseller'=>'Đại lý bán hàng',
+	'LBL_START_DATE' => 'Ngày bắt đầu',
+	'LBL_END_DATE' => 'Ngày kết thúc',
+	
+	//Duplication error message
+	'LBL_DUPLICATES_EXIST' => 'Tên tổ chức này đã tồn tại',
+	'LBL_COPY_SHIPPING_ADDRESS' => 'Sao chép địa chỉ vận chuyển',
+	'LBL_COPY_BILLING_ADDRESS' => 'Sao chép địa chỉ thanh toán',
+);
+
+$jsLanguageStrings = array(
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Bạn có chắc chắn muốn xóa?',
+	'LBL_DELETE_CONFIRMATION' => 'Nếu xoá tổ chức này, những cơ hội và báo giá liên quan đến nó cũng sẽ bị xoá. Bạn có chắc muốn xoá tổ chức này?',
+	'LBL_MASS_DELETE_CONFIRMATION' => ' Nếu xoá những tổ chức này, những cơ hội và báo giá liên quan đến những tổ chức này cũng sẽ bị xoá. Bạn có chắc muốn xoá tổ chức này?',
+	'JS_DUPLICTAE_CREATION_CONFIRMATION' => 'Tên tổ chức đã tồn tại. Bạn có muốn tạo bản ghi lặp?'
+);
